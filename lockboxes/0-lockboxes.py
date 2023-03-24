6ac2d87b26a7f5344b8790bed59b3e808963dce2
MedAliKrifa <daliolimona@gmail.com>
2023-03-24 19:27:13 +0100

    0-lockboxes.py

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -2,6 +2,8 @@
 """
     Box opener method.
 """
+
+
 def canUnlockAll(boxes):
     # Initialize a set to keep track of the boxes we have access to
     # We start with the first box (index 0) already open
@@ -13,16 +15,13 @@ def canUnlockAll(boxes):
     # Add all the keys in the first box to the unused_keys list
     unused_keys.extend(boxes[0])
 
-    # Loop until we've tried all the unused keys and haven't opened any new boxes
     while unused_keys:
         # Take a key from the unused_keys list
         key = unused_keys.pop(0)
 
-        # If the key opens a box we haven't already opened, add it to the open_boxes set
         if key < len(boxes) and key not in open_boxes:
             open_boxes.add(key)
             
-            # Add all the keys in the newly opened box to the unused_keys list
             unused_keys.extend(boxes[key])
 
     # If we've opened all the boxes, return True, else return False

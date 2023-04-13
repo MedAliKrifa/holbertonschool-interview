f31d43be64297fb5c675298038961be1f67764fa
MedAliKrifa <daliolimona@gmail.com>
2023-04-13 01:46:48 +0100

    0-minoperations.py

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -5,15 +5,16 @@ Minimum Operations
 
 
 def minOperations(n):
-    i = 0
-    n_div = 2
-    if n < 1 or type(n) is not int:
+    if n == 1:
         return 0
-    while n > 1:
-        if n % n_div == 0:
-            n = n / n_div
-            i = i + n_div
-        else:
-            n_div = n_div + 1
-    return
+    
+    dp = [float('inf')] * (n+1)
+    dp[1] = 0
+    
+    for i in range(2, n+1):
+        for j in range(1, i):
+            if i % j == 0:
+                dp[i] = min(dp[i], dp[j] + (i // j))
+    
+    return dp[n] if dp[n] != float('inf') else 0
 

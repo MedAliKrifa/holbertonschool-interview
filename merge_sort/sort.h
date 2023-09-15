a90f68966b4054442c7f6fc0931db4190833e978
MedAliKrifa <daliolimona@gmail.com>
2023-09-15 01:28:15 +0100

    sort.h

--- merge_sort/sort.h
+++ merge_sort/sort.h
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 
 void print_array(const int *array, size_t size);
-void merge(int *array, int *left, int left_size, int *right, int right_size);
-void merge_sort(int *array, size_t size);
+void merge(int *array, int start, int middle, int end, int *temp);
+void merge_sort_recursive(int *array, int start_idx, int end_idx, int *tmp);
+void merge_sort(int *array, int size);
+
 #endif /* SORT_H */

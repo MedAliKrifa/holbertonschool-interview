c975a230e56543d12e9235c80c89fda160ac05f3
MedAliKrifa <daliolimona@gmail.com>
2023-09-15 23:56:26 +0100

    sort.h

--- merge_sort/sort.h
+++ merge_sort/sort.h
@@ -1,12 +1,9 @@
-#ifndef SORT_H
-#define SORT_H
-
+#ifndef HEADER
+#define HEADER
 #include <stdio.h>
 #include <stdlib.h>
 
 void print_array(const int *array, size_t size);
-void merge(int *array, int start, int middle, int end, int *temp);
-void merge_sort_recursive(int *array, int start_idx, int end_idx, int *tmp);
-void merge_sort(int *array, int size);
+void merge_sort(int *array, size_t size);
 
-#endif /* SORT_H */
+#endif

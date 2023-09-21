d46fbc1eeb6d2b84fd16825d93af683c7f083ff9
MedAliKrifa <daliolimona@gmail.com>
2023-09-21 20:33:59 +0100

    0-making_change.py

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -3,24 +3,18 @@
 make change: bottom up method
 """
 
+
 def makeChange(coins, total):
     """
     make change: bottom up method
     """
     if total < 0:
-        return 0  # If total is negative, return 0 as specified
-
-    # Initialize a list to store the minimum number of coins needed to make each total from 0 to 'total'
+        return 0
     dp = [float('inf')] * (total + 1)
-    dp[0] = 0  # 0 coins are needed to make a total of 0
-
-    # Iterate through each coin and update the dp table
+    dp[0] = 0
     for coin in coins:
         for sub_total in range(coin, total + 1):
             dp[sub_total] = min(dp[sub_total], dp[sub_total - coin] + 1)
-
-    # If dp[total] is still equal to float('inf'), it means the total cannot be met
     if dp[total] == float('inf'):
         return -1
-
     return dp[total]

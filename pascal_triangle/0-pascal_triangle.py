c8692ad3fea5c006c636c271c824330741abd4f5
MedAliKrifa <daliolimona@gmail.com>
2023-11-10 17:28:03 +0100

    0-pascal_triangle.py

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,14 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = []
+    for i in range(n):
+        row = [1]  # First element of each row is always 1
+        if triangle:
+            last_row = triangle[-1]
+            row.extend([sum(pair) for pair in zip(last_row, last_row[1:])])
+            row.append(1)  # Last element of each row is always 1
+        triangle.append(row)
+
+    return triangle

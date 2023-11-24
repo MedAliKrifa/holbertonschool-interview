57db090fdc34558ab9d55fcebbc79802588d8f86
MedAliKrifa <daliolimona@gmail.com>
2023-11-24 01:00:48 +0100

    substring.h

---
+++ substring/substring.h
@@ -0,0 +1,7 @@
+#ifndef substring_H
+#define substring_H
+
+int allWordsCovered(int *wordCount, int nb_words);
+int *find_substring(char const *s, char const **words, int nb_words, int *n);
+
+#endif

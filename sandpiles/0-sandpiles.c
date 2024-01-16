4ec9a47be71dd71a80f1757d5f8a23053ede6e79
MedAliKrifa <daliolimona@gmail.com>
2024-01-16 23:45:57 +0100

    chang

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -1,102 +1,109 @@
 #include "sandpiles.h"
 /**
- * printSandpile - function to print the sandpile
+ * print_grid - Print 3x3 grid
  * @grid: 2D array 3*3
  *
  */
-void printSandpile(int grid[3][3])
-{
-printf("Sandpile:\n");
-for (int i = 0; i < 3; i++)
-{
-for (int j = 0; j < 3; j++)
-{
-printf("%d ", grid[i][j]);
-}
-printf("\n");
-}
-printf("\n");
-}
 
+static void print_grid(int grid[3][3])
+{
+    int i, j;
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (j)
+                printf(" ");
+            printf("%d", grid[i][j]);
+        }
+        printf("\n");
+    }
+}
 
 /**
- * isStable - function to check if the sandpile is stable
- * @grid: 2D array 3*3
+ * copy - copy of two sandpiles
+ * @scr: 2D array
+ * @des: 2D array
+ */
+
+void copy(int scr[3][3], int des[3][3])
+{
+    int i, j;
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            des[i][j] = scr[i][j];
+        }
+    }
+}
+
+/**
+ * verif - test
+ * @grid: 2D array
  * Return: 0
  */
-int isStable(int grid[3][3])
+
+int verif(int grid[3][3])
 {
-for (int i = 0; i < 3; i++)
-{
-for (int j = 0; j < 3; j++)
-{
-if (grid[i][j] > 3)
-{
-return (0);
-}
-}
-}
-return (1);
+    int i, j;
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (grid[i][j] > 3)
+                return (0);
+        }
+    }
+    return (1);
 }
 
 /**
- * topple - function to topple the sandpile
- * @grid: 2D array 3*3
- *
- */
-void topple(int grid[3][3])
-{
-int temp[3][3];
-memcpy(temp, grid, sizeof(temp));
-for (int i = 0; i < 3; i++)
-{
-for (int j = 0; j < 3; j++)
-{
-if (temp[i][j] > 3)
-{
-grid[i][j] -= 4;
-if (i - 1 >= 0)
-{
-grid[i - 1][j]++;
-}
-if (i + 1 < 3)
-{
-grid[i + 1][j]++;
-}
-if (j - 1 >= 0)
-{
-grid[i][j - 1]++;
-}
-if (j + 1 < 3)
-{
-grid[i][j + 1]++;
-}
-}
-}
-}
-}
-
-/**
- * sandpiles_sum - function to compute the sum of two sandpiles
- * @grid1: 2D array 3*3
- * @grid2: 2D array 3*3
+ * sandpiles_sum - function that computes the sum of two sandpiles
+ * @grid1: 2D array
+ * @grid2: 2D array
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
+    int x;
+    int y;
+    int referenceGrid[3][3];
+    int type = 0;
 
-for (int i = 0; i < 3; i++)
-{
-for (int j = 0; j < 3; j++)
-{
-grid1[i][j] += grid2[i][j];
-}
-}
-
-
-while (!isStable(grid1))
-{
-printSandpile(grid1);
-topple(grid1);
-}
-printSandpile(grid1);
+    for (x = 0; x < 3; x++)
+        for (y = 0; y < 3; y++)
+            grid1[x][y] = grid1[x][y] + grid2[x][y];
+    type = verif(grid1);
+    if (type == 1)
+        return;
+    printf("=\n");
+    print_grid(grid1);
+    while (type == 0)
+    {
+        copy(grid1, referenceGrid);
+        for (x = 0; x < 3; x++)
+            for (y = 0; y < 3; y++)
+            {
+                if (referenceGrid[x][y] > 3)
+                {
+                    grid1[x][y] = grid1[x][y] - 4;
+                    if (x - 1 >= 0)
+                        grid1[x - 1][y] += 1;
+                    if (x + 1 <= 2)
+                        grid1[x + 1][y] += 1;
+                    if (y - 1 >= 0)
+                        grid1[x][y - 1] += 1;
+                    if (y + 1 <= 2)
+                        grid1[x][y + 1] += 1;
+                }
+            }
+        type = verif(grid1);
+        if (type == 1)
+            break;
+        printf("=\n");
+        print_grid(grid1);
+    }
 }

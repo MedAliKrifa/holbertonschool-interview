89f65ca56a8191831119ce237ccc74ff2b8226fb
MedAliKrifa <daliolimona@gmail.com>
2024-01-16 23:50:51 +0100

    chang

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -1,17 +1,18 @@
 #include "sandpiles.h"
+
 /**
- * print_grid - Print 3x3 grid
- * @grid: 2D array 3*3
+ * _print_grid - Prints a sand pile grid
+ * @grid: Grid to print
  *
+ * Return: No Return
  */
-
-static void print_grid(int grid[3][3])
+void _print_grid(int grid[3][3])
 {
-    int i, j;
+    int i, j, limit = 3;
 
-    for (i = 0; i < 3; i++)
+    for (i = 0; i < limit; i++)
     {
-        for (j = 0; j < 3; j++)
+        for (j = 0; j < limit; j++)
         {
             if (j)
                 printf(" ");
@@ -22,88 +23,90 @@ static void print_grid(int grid[3][3])
 }
 
 /**
- * copy - copy of two sandpiles
- * @scr: 2D array
- * @des: 2D array
+ * grid_sum - Computes the sum of two sandpiles
+ * @grid1: Pile 1
+ * @grid2: Pile 2
+ *
+ * Return: No Return
  */
-
-void copy(int scr[3][3], int des[3][3])
+void grid_sum(int grid1[3][3], int grid2[3][3])
 {
-    int i, j;
+    int i, j, limit = 3;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            des[i][j] = scr[i][j];
-        }
-    }
+    for (i = 0; i < limit; i++)
+        for (j = 0; j < limit; j++)
+            grid1[i][j] += grid2[i][j];
 }
 
 /**
- * verif - test
- * @grid: 2D array
- * Return: 0
+ * is_stable - Checks if sand pile is stable
+ * @grid: Grid to check
+ *
+ * Return: No Return
  */
-
-int verif(int grid[3][3])
+int is_stable(int grid[3][3])
 {
-    int i, j;
+    int i, j, limit = 3;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (grid[i][j] > 3)
+    for (i = 0; i < limit; i++)
+        for (j = 0; j < limit; j++)
+            if (grid[i][j] > limit)
                 return (0);
-        }
-    }
+
     return (1);
 }
 
 /**
- * sandpiles_sum - function that computes the sum of two sandpiles
- * @grid1: 2D array
- * @grid2: 2D array
+ * toppling - Distributes sand grains
+ * @grid: Grid to process
+ *
+ * Return: No Return
+ */
+void toppling(int grid[3][3])
+{
+    int i, j, limit = 3, wall = 2;
+
+    int grid_tmp[3][3] = {
+        {0, 0, 0},
+        {0, 0, 0},
+        {0, 0, 0}};
+
+    for (i = 0; i < limit; i++)
+    {
+        for (j = 0; j < limit; j++)
+        {
+            if (grid[i][j] > limit)
+            {
+                (i - 1) >= 0 ? grid_tmp[i - 1][j]++ : 0;
+
+                (i + 1) <= wall ? grid_tmp[i + 1][j]++ : 0;
+
+                (j - 1) >= 0 ? grid_tmp[i][j - 1]++ : 0;
+
+                (j + 1) <= wall ? grid_tmp[i][j + 1]++ : 0;
+
+                grid[i][j] -= 4;
+            }
+        }
+    }
+
+    grid_sum(grid, grid_tmp);
+}
+
+/**
+ * sandpiles_sum - Computes the sum of two sandpiles
+ * @grid1: Pile 1
+ * @grid2: Pile 2
+ *
+ * Return: No Return
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-    int x;
-    int y;
-    int referenceGrid[3][3];
-    int type = 0;
-
-    for (x = 0; x < 3; x++)
-        for (y = 0; y < 3; y++)
-            grid1[x][y] = grid1[x][y] + grid2[x][y];
-    type = verif(grid1);
-    if (type == 1)
-        return;
-    printf("=\n");
-    print_grid(grid1);
-    while (type == 0)
+    grid_sum(grid1, grid2);
+    while (!is_stable(grid1))
     {
-        copy(grid1, referenceGrid);
-        for (x = 0; x < 3; x++)
-            for (y = 0; y < 3; y++)
-            {
-                if (referenceGrid[x][y] > 3)
-                {
-                    grid1[x][y] = grid1[x][y] - 4;
-                    if (x - 1 >= 0)
-                        grid1[x - 1][y] += 1;
-                    if (x + 1 <= 2)
-                        grid1[x + 1][y] += 1;
-                    if (y - 1 >= 0)
-                        grid1[x][y - 1] += 1;
-                    if (y + 1 <= 2)
-                        grid1[x][y + 1] += 1;
-                }
-            }
-        type = verif(grid1);
-        if (type == 1)
-            break;
         printf("=\n");
-        print_grid(grid1);
+        _print_grid(grid1);
+        toppling(grid1);
     }
 }

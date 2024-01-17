14738bb36692d859c7b6b52d184a552c0a055639
MedAliKrifa <daliolimona@gmail.com>
2024-01-17 21:44:30 +0100

    0-starwars_characters.js

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,36 +1,33 @@
 #!/usr/bin/node
-const argv = process.argv;
-const urlFilm = 'https://swapi-api.hbtn.io/api/films/';
-const urlMovie = `${urlFilm}${argv[2]}/`;
-
 const request = require('request');
 
-request(urlMovie, function (error, response, body) {
-  if (error == null) {
-    const fbody = JSON.parse(body);
-    const characters = fbody.characters;
+const Args = process.argv.slice(2);
+const MovieID = Args[0];
 
-    if (characters && characters.length > 0) {
-      const limit = characters.length;
-      CharRequest(0, characters[0], characters, limit);
+const options = {
+  url: `https://swapi-api.hbtn.io/api/films/${MovieID}/`,
+  json: true,
+  transform (body, response) {
+    if (response.headers['content-type'] === 'application/json') {
+      response.body = JSON.parse(body);
     }
-  } else {
-    console.log(error);
+    return response;
+  }
+};
+
+request(options, async (error, response, body) => {
+  if (error) {
+    throw new Error(`HTTP error: ${response.status}`);
+  }
+  for (const characterURL of response.body.characters) {
+    await new Promise((resolve, reject) => {
+      request(characterURL, (err, res, body1) => {
+        if (err) {
+          reject(new Error(`HTTP error: ${res.status}`));
+        }
+        console.log(JSON.parse(body1).name);
+        resolve();
+      });
+    });
   }
 });
-
-function CharRequest (idx, url, characters, limit) {
-  if (idx === limit) {
-    return;
-  }
-  request(url, function (error, response, body) {
-    if (!error) {
-      const rbody = JSON.parse(body);
-      console.log(rbody.name);
-      idx++;
-      CharRequest(idx, characters[idx], characters, limit);
-    } else {
-      console.error('error:', error);
-    }
-  });
-}
